33b07388be60bc99fa9cea0d89e869aab7331e8b
anne <richardson.ae@gmail.com>
2015-06-06 21:14:41 -0500

    deleted devise

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,12 @@ Rails.application.routes.draw do
 
   get 'spread/spread3'
 
-  devise_for :users
   resources :cards
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'cards#index'
+root 'page#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
