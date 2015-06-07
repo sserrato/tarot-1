9d27151346043b579e0d3c670274ebc4f955e83f
anne <richardson.ae@gmail.com>
2015-06-06 19:28:16 -0500

    installed gem annotate

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -5,3 +5,17 @@ class CardTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+#
+# Table name: cards
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  theme      :string
+#  meaning    :text
+#  imagefile  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  arcana     :string
+#
