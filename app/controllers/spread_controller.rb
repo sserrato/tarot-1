eb5f3fafab4dd12b1a53edf1a9054fa72b9cace4
anne <richardson.ae@gmail.com>
2015-06-15 17:18:50 -0500

    removed params so i can relaunch to heroku

--- app/controllers/spread_controller.rb
+++ app/controllers/spread_controller.rb
@@ -5,18 +5,10 @@ class SpreadController < ApplicationController
 
     @reading = []
 
-    if params[:you] && params[:partner] && params[:relation]
-      @reading = [ Card.find(params[:you]), 
-        Card.find(params[:partner]), 
-        Card.find(params[:relation]) ]
-
-    else
       @cards.sample(3).each do |card|
         @reading.push(card) #pushes 3 card objects into this instance array
       end #do
-    end #if
-
-    @link = "?you=#{@reading[0].id}&partner=#{@reading[1].id}&relation#{@reading[2].id}"
+    
   end #spread1
 
   def spread2
