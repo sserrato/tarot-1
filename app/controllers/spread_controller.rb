8d835bfd9867bb7f863f2dce5cd8770603d7e6c0
anne <richardson.ae@gmail.com>
2015-06-15 13:45:22 -0500

    made params changes before going to a feature branch

--- app/controllers/spread_controller.rb
+++ app/controllers/spread_controller.rb
@@ -4,10 +4,19 @@ class SpreadController < ApplicationController
     @cards = Card.all
 
     @reading = []
-    @cards.sample(3).each do |card|
-      @reading.push(card) #pushes 3 card objects into this instance array
-    end
 
+    if params[:you] && params[:partner] && params[:relation]
+      @reading = [ Card.find(params[:you]), 
+        Card.find(params[:partner]), 
+        Card.find(params[:relation]) ]
+
+    else
+      @cards.sample(3).each do |card|
+        @reading.push(card) #pushes 3 card objects into this instance array
+      end #do
+    end #if
+
+    @link = "?you=#{@reading[0].id}&partner=#{@reading[1].id}&relation#{@reading[2].id}"
   end #spread1
 
   def spread2
