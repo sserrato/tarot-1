6561fbe48b07b7a57fcffac892f0c7227ae83ac9
anne <richardson.ae@gmail.com>
2016-01-15 09:53:04 -0600

    commented out unused testing code

--- test/controllers/spread_controller_test.rb
+++ test/controllers/spread_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class SpreadControllerTest < ActionController::TestCase
-  test "should get spread1" do
-    get :spread1
-    assert_response :success
-  end
+  # test "should get spread1" do
+  #   get :spread1
+  #   assert_response :success
+  # end
 
   # test "should get spread2" do
   #   get :spread2
